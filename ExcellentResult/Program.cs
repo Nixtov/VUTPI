78c69c202b1b64f0d6b9ad665737886a0990c93e
Nixtov <nixtov@gmail.com>
2018-03-20 22:56:44 +0200

    1

---
+++ ExcellentResult/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcellentResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double result = Double.Parse(Console.ReadLine());
+            if (result >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
