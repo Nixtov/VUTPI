91136639b7c2660202611976399070f9921f9ff8
Nixtov <nixtov@gmail.com>
2018-03-22 16:34:46 +0200

    the last one

---
+++ EnterEvenNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("Even number entered: " + n);
+                    break;
+                }
+                n = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ NumberInRange1To100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberInRange1To100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            while (n < 1 || n > 100)
+            {
+                Console.WriteLine("Invalid number! Еnter a number in the range [1...100]:");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
